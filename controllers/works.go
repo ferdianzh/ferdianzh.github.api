10b1bdd9ec1533f99ac114a874934620bc0c5fff
ferdianzh <ferdianzh30@gmail.com>
2022-01-30 08:50:39 +0700

    work crud commit

--- controllers/works.go
+++ controllers/works.go
@@ -7,13 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type WorksValidation struct {
-	IDRole      string `json:"role" binding:"required"`
-	Name 			string `json:"name" binding:"required"`
-	Tech        string `json:"tech" binding:"required"`
-	Description string `json:"description"`
-}
-
 // GET /works
 // Get all works
 func FindWorks(c *gin.Context) {
@@ -23,10 +16,16 @@ func FindWorks(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": works})
 }
 
+type CreateWorkInput struct {
+	IDRole      string `json:"role" binding:"required"`
+	Name 			string `json:"name" binding:"required"`
+	Tech        string `json:"tech" binding:"required"`
+	Description string `json:"description"`
+}
 // POST /works
 // Create new works
 func CreateWorks(c *gin.Context) {
-	var input WorksValidation
+	var input CreateWorkInput
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -41,4 +40,61 @@ func CreateWorks(c *gin.Context) {
 	models.DB.Create(&work)
 
 	c.JSON(http.StatusOK, gin.H{"data": work})
+}
+
+// GET /works/:id
+// Find a work
+func FindWork(c *gin.Context) {
+	var work models.Work
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&work).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "record not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": work})
+}
+
+type UpdateWorkInput struct {
+	IDRole      string `json:"role"`
+	Name 			string `json:"name"`
+	Tech        string `json:"tech"`
+	Description string `json:"description"`
+}
+// PATCH /works/:id
+// Update a work
+func UpdateWork(c *gin.Context) {
+	var work models.Work
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&work).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "record not found"})
+		return
+	}
+
+	var input UpdateWorkInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&work).Updates(models.Work{
+		IDRole		: input.IDRole,
+		Name			: input.Name,
+		Tech			: input.Tech,
+		Description	: input.Description,
+	})
+
+	c.JSON(http.StatusOK, gin.H{"data": work})
+}
+
+// DELETE /works/:id
+// Delete a work
+func DeleteWork(c *gin.Context) {
+	var work models.Work
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&work).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "record not found"})
+		return
+	}
+
+	models.DB.Delete(&work)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
