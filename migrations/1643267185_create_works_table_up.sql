57450bb78335f06fa661304a3d2309c94a2996f1
ferdianzh <ferdianzh30@gmail.com>
2022-01-30 09:24:28 +0700

    migrations commit

--- migrations/1643267185_create_works_table_up.sql
+++ migrations/1643267185_create_works_table_up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS works (
-   "id" int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   "id_role" int NOT NULL FOREIGN KEY REFERENCES "roles(id)",
-   "name" varchar(50),
-   "tech" varchar(50),
-   "description" varchar(255),
+   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   id_role int NOT NULL,
+   name varchar(50),
+   tech varchar(50),
+   description varchar(255),
+   image varchar(50),
+   FOREIGN KEY (id_role) REFERENCES roles(id)
 ) ENGINE=INNODB;
 
 COMMIT;
